65cdd032765b27e61715f999909c4d40e32311e2
jonathanhans22 <jonathanhans.2205@gmail.com>
2026-07-02 10:06:39 +0700

    SELESAI

--- logistik-tracker/src/supabaseClient.js
+++ logistik-tracker/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://rwpykupyieqioiyhcnpz.supabase.co'; // Hasil salinan baris pertama
-const supabaseKey = 'sb_publishable_jYHlhiz0HDsqKH25tZhKmA_Icc-zQeG'; // Hasil salinan teks panjang di baris kedua
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3cHlrdXB5aWVxaW9peWhjbnB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5MTk2MDgsImV4cCI6MjA5ODQ5NTYwOH0.Bz4XSBH-qzaAAyO2PuIhGsN5KhPk5fGx86yL0KB62_M-zQeG'; // Hasil salinan teks panjang di baris kedua
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
